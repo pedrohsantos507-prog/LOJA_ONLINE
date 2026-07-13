d7620f1d866f24a94cccd045c8b55f83badb0643
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-07-13 20:34:40 -0300

    criei novas pastas

---
+++ JS/carrinho.js
@@ -0,0 +1,7 @@
+//VERIFICANDO SE JA EXISTE UM CARRINHO SALVO
+let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+//CRIANDO A PRIMEIRA FUNÇÃO
+const adicionarAocarrinho = (produto) =>
+{
+    
+}
--- JS/produtos.js
+++ JS/produtos.js
@@ -1,141 +1,141 @@
 const produtos = [
   {
     id_produto: 1,
-    descricao_produto: "Notebook Dell Inspiron 15 i5 16GB 512GB SSD",
-    caminho_imagem: "imagens/images (8).jpg",
+    descricao_produto: "Computador Dell, intel 7 10° Geração",
+    caminho_imagem: "imagens/imagens (1).jpg",
     valor_unitario: 4299.90,
     id_secao: 1
   },
   {
     id_produto: 2,
-    descricao_produto: "Notebook Lenovo IdeaPad 3 Ryzen 7",
-    caminho_imagem: "imagens/images (9).jpg",
+    descricao_produto: "Computador gamer ryzen 9 9700x, SSD 1TB, nvidia 5060 ti 16 gb ",
+    caminho_imagem: "imagens/images (2).jpg",
     valor_unitario: 3999.90,
     id_secao: 1
   },
   {
     id_produto: 3,
-    descricao_produto: "Monitor LG UltraWide 29",
-    caminho_imagem: "imagens/images (6).jpg",
+    descricao_produto: "Kit Gamer",
+    caminho_imagem: "imagens/images (3).jpg",
     valor_unitario: 1199.90,
     id_secao: 2
   },
   {
     id_produto: 4,
-    descricao_produto: "Monitor Samsung Odyssey G5",
-    caminho_imagem: "imagens/images (11).jpg",
+    descricao_produto: "Setup Gamer Completo",
+    caminho_imagem: "imagens/images (4).jpg",
     valor_unitario: 1899.90,
     id_secao: 2
   },
   {
     id_produto: 5,
-    descricao_produto: "Teclado Mecânico Redragon Kumara",
-    caminho_imagem: "imagens/images (12).jpg",
+    descricao_produto: "Computador Gamer, intel 9 14° Geração, Nvidia 5080 ti 16 gb",
+    caminho_imagem: "imagens/images (5).jpg",
     valor_unitario: 249.90,
     id_secao: 3
   },
   {
     id_produto: 6,
-    descricao_produto: "Mouse Logitech G502 Hero",
-    caminho_imagem: "imagens/images.jpg",
+    descricao_produto: "Computador Retro",
+    caminho_imagem: "imagens/images (6).jpg",
     valor_unitario: 289.90,
     id_secao: 3
   },
   {
     id_produto: 7,
-    descricao_produto: "Headset HyperX Cloud Stinger",
-    caminho_imagem: "imagens/images (14).jpg",
+    descricao_produto: "Computador Gamer ryzen 7 9800x, Nvidia 4090 ti 24 gb, com kitde fans",
+    caminho_imagem: "imagens/images (7).jpg",
     valor_unitario: 279.90,
     id_secao: 3
   },
   {
     id_produto: 8,
-    descricao_produto: "SSD Kingston NV2 1TB",
-    caminho_imagem: "imagens/images (1).jpg",
+    descricao_produto: "notebook Lenovo ideaPad 3 Ryzen 7",
+    caminho_imagem: "imagens/images (8).jpg",
     valor_unitario: 399.90,
     id_secao: 4
   },
   {
     id_produto: 9,
-    descricao_produto: "Memória RAM Corsair 16GB",
-    caminho_imagem: "imagens/imagens (1).jpg",
+    descricao_produto: "kit de Perifericos",
+    caminho_imagem: "imagens/images (9).jpg",
     valor_unitario: 319.90,
     id_secao: 4
   },
   {
     id_produto: 10,
-    descricao_produto: "Placa de Vídeo RTX 4060",
-    caminho_imagem: "imagens/images (8).jpg",
+    descricao_produto: "Computador Gamer, Intel 9 15° Geração, amd 9800xt 16gb ",
+    caminho_imagem: "imagens/images (10).jpg",
     valor_unitario: 2399.90,
     id_secao: 4
   },
   {
     id_produto: 11,
-    descricao_produto: "Processador Intel Core i5-14400F",
-    caminho_imagem: "imagens/images (9).jpg",
+    descricao_produto: "Notebook Nitro Acer Ryzen 7 5700gt, Nvidia 3050 8gb",
+    caminho_imagem: "imagens/images (11).jpg",
     valor_unitario: 1299.90,
     id_secao: 4
   },
   {
     id_produto: 12,
-    descricao_produto: "Processador AMD Ryzen 7 5700X",
-    caminho_imagem: "imagens/images (6).jpg",
+    descricao_produto: "Computador De Biblioteca Antigo",
+    caminho_imagem: "imagens/images (12).jpg",
     valor_unitario: 1199.90,
     id_secao: 4
   },
   {
     id_produto: 13,
-    descricao_produto: "Impressora Epson EcoTank",
-    caminho_imagem: "imagens/images (11).jpg",
+    descricao_produto: "Setup Gamer Completo",
+    caminho_imagem: "imagens/images (1).jpg",
     valor_unitario: 1099.90,
     id_secao: 5
   },
   {
     id_produto: 14,
-    descricao_produto: "Impressora HP LaserJet",
-    caminho_imagem: "imagens/images (12).jpg",
+    descricao_produto: "Notebook Lenovo, Win 11 Pro",
+    caminho_imagem: "imagens/images (14).jpg",
     valor_unitario: 1399.90,
     id_secao: 5
   },
   {
     id_produto: 15,
-    descricao_produto: "Webcam Logitech C920",
-    caminho_imagem: "imagens/images.jpg",
+    descricao_produto: "Kit Gamer ",
+    caminho_imagem: "imagens/images (15).jpg",
     valor_unitario: 449.90,
     id_secao: 5
   },
   {
     id_produto: 16,
-    descricao_produto: "Roteador TP-Link Archer C6",
-    caminho_imagem: "imagens/images (14).jpg",
+    descricao_produto: "Computador Gamer Ryzen 9 9900x, Nvidia 4090 ti 24 gb, SSD 5tb",
+    caminho_imagem: "imagens/images (16).jpg",
     valor_unitario: 299.90,
     id_secao: 5
   },
   {
     id_produto: 17,
     descricao_produto: "Gabinete Gamer Rise Mode",
-    caminho_imagem: "imagens/images (1).jpg",
+    caminho_imagem: "imagens/images (17).jpg",
     valor_unitario: 349.90,
     id_secao: 4
   },
   {
     id_produto: 18,
-    descricao_produto: "Fonte Corsair CV650",
-    caminho_imagem: "imagens/imagens (1).jpg",
+    descricao_produto: "Gabinete Gamer, Sem fans,logitech",
+    caminho_imagem: "imagens/images (18).jpg",
     valor_unitario: 429.90,
     id_secao: 4
   },
   {
     id_produto: 19,
-    descricao_produto: "Cadeira Gamer ThunderX3",
-    caminho_imagem: "imagens/images (8).jpg",
+    descricao_produto: "Kit Gamer Pichau",
+    caminho_imagem: "imagens/images.(19).jpg",
     valor_unitario: 999.90,
     id_secao: 5
   },
   {
     id_produto: 20,
-    descricao_produto: "Mouse Pad Gamer RGB",
-    caminho_imagem: "imagens/images (9).jpg",
+    descricao_produto: "Monitor Curvo Samsumg, 240Hz, 32 pol",
+    caminho_imagem: "imagens/images.jpg",
     valor_unitario: 129.90,
     id_secao: 3
   }
